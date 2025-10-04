ff77765dea997e7adaefe0380bb88e125c08d993
Prasun Bhuit <prasun@spcpl.co.in>
2025-10-04 18:31:49 +0530

    commit from office on 4 oct evening

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import learningResources from "./components/learningResources.vue";
+  const storedResources = [
+    {
+      id: "vuejs-official",
+      title: "The Vue JS",
+      description: "The official documentation of Vue JS",
+      links: "https://vuejs.org",
+    },
+    {
+      id: "search-engine",
+      title: "Google",
+      description: "Easy way the search the web",
+      links: "https://google.com",
+    },
+  ];
+</script>
+
+<template>
+  <ul>
+    <learningResources :storedResources="storedResources" />
+  </ul>
+</template>
+
+<style scoped></style>
---
+++ src/components/learningResources.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  const props = defineProps({ storedResources: { type: Array } });
+</script>
+<template>
+  <div>
+    <li
+      v-for="list in props"
+      :key="list.id"
+    >
+      {{ list.title }}
+    </li>
+  </div>
+</template>
+
+<style scoped></style>
